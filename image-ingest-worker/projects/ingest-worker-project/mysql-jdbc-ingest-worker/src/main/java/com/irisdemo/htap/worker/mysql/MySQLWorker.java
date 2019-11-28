78be322f468a89845a9a777a08dedff3683a9113
Amir Samary <amirsamary@gmail.com>
2019-11-28 12:39:27 -0500

    Added support for MySQL

---
+++ image-ingest-worker/projects/ingest-worker-project/mysql-jdbc-ingest-worker/src/main/java/com/irisdemo/htap/worker/mysql/MySQLWorker.java
@@ -0,0 +1,166 @@
+package com.irisdemo.htap.worker.mysql;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.irisdemo.htap.config.Config;
+import com.irisdemo.htap.workersrv.IWorker;
+import com.irisdemo.htap.workersrv.WorkerMetricsAccumulator;
+import com.irisdemo.htap.workersrv.WorkerSemaphore;
+
+@Component("worker")
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class MySQLWorker implements IWorker 
+{
+	protected static Logger logger = LoggerFactory.getLogger(MySQLWorker.class);
+	
+    @Autowired
+    protected WorkerSemaphore workerSemaphore;
+    
+    @Autowired 
+    protected WorkerMetricsAccumulator accumulatedMetrics;
+    
+    @Autowired
+    protected Config config;    
+    
+    @Autowired
+    protected WorkerDBUtils workerDBUtils;
+    
+	protected static char[] prefixes = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+	@Async
+    public CompletableFuture<Long> startOneFeed(String nodePrefix, int threadNum) throws IOException, SQLException
+    {	
+		long recordNum = 0;
+		long batchSizeInBytes;
+		
+		Connection connection = workerDBUtils.getDataSource().getConnection();
+    	
+		logger.info("Ingestion worker #"+threadNum+" started.");
+		
+		connection.setAutoCommit(false);
+		
+		PreparedStatement preparedStatement = connection.prepareStatement(config.getInsertStatement());
+
+		int parameterCount = preparedStatement.getParameterMetaData().getParameterCount();
+		
+		workerDBUtils.initializeRandomMapping(connection);
+		
+    	try
+    	{    		
+    		String threadPrefix = nodePrefix+prefixes[threadNum];
+    		
+    		int currentBatchSize;
+        	
+	    	while(workerSemaphore.green())
+	    	{
+	    		currentBatchSize = 0;
+	    		batchSizeInBytes = 0;
+	    		
+	    		while(workerSemaphore.green())
+	    		{
+	    			if (currentBatchSize==config.getIngestionBatchSize()) 
+	    				break;
+	    			
+	    			batchSizeInBytes+= WorkerDBUtils.pupulatePreparedStatement(parameterCount, ++recordNum, threadPrefix, preparedStatement);
+	    		
+	    			preparedStatement.addBatch();
+	    			preparedStatement.clearParameters();
+	    			currentBatchSize++;	    			
+	    		}
+
+				if(workerSemaphore.green())
+				{
+					preparedStatement.executeBatch();
+					preparedStatement.clearBatch();
+					connection.commit();
+					accumulatedMetrics.addToStats(currentBatchSize, batchSizeInBytes);
+				}
+	    	}	
+			
+		} 
+    	catch (SQLException sqlException) 
+    	{
+			throw sqlException;
+		} 
+    	finally
+    	{
+    		connection.close();
+    	}
+    	
+    	logger.info("Ingestion worker #"+threadNum+" finished.");
+    	return CompletableFuture.completedFuture(recordNum);
+	}
+	
+	@Override
+	public void prepareDatabaseForSpeedTest() throws Exception
+    {
+		Connection connection = workerDBUtils.getDataSource().getConnection();
+		
+		try
+		{
+			try
+			{
+				workerDBUtils.dropTable(connection);
+			}
+			catch (SQLException exception)
+			{
+				if (exception.getErrorCode()==30) //Table or view not found
+				{
+					
+				}
+				else if (exception.getMessage().startsWith("Unknown table"))
+				{
+					
+				}
+				else
+				{
+					throw exception;
+				}
+				
+			}
+			
+			workerDBUtils.createTable(connection);
+		}
+		catch (Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
+			connection.close();
+		}
+    	
+    }
+	
+	@Override
+	public void truncateTable() throws Exception
+    {
+		Connection connection = workerDBUtils.getDataSource().getConnection();
+		
+		try
+		{
+			workerDBUtils.truncateTable(connection);
+		}
+		catch (Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
+			connection.close();
+		}
+    	
+    }
+}
--- run.sh
+++ run.sh
@@ -2,6 +2,21 @@
 #
 # This script is meant to support running the speedtest with docker-compose
 #
-docker-compose stop
-docker-compose rm -f
-docker-compose up
+# You can:
+# - call it without any arguments to run the speed test using IRIS. 
+# - call it with an argument to run the speed test using another datbase. The available options are: 
+#   - mysql
+#
+# Each option will lead to using a diffent docker-compose.yml file.
+#
+
+if [ -z "$1" ];
+then
+    docker-compose stop
+    docker-compose rm -f
+    docker-compose up
+else
+    docker-compose -f $1-docker-compose.yml stop
+    docker-compose -f $1-docker-compose.yml rm -f
+    docker-compose -f $1-docker-compose.yml up
+fi
