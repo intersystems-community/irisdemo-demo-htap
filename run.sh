1df905a87ec5ef9e6f0a5b36eb279f6f87ddb81f
Amir Samary <amirsamary@gmail.com>
2019-12-09 21:10:29 -0500

    Adding support for SAP HANA

--- run.sh
+++ run.sh
@@ -10,13 +10,35 @@
 # Each option will lead to using a diffent docker-compose.yml file.
 #
 
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
 if [ -z "$1" ];
 then
     docker-compose stop
     docker-compose rm -f
     docker-compose up
 else
+    if [ "$1" == "hana" ];
+    then
+        # Following instructions on https://hub.docker.com/_/sap-hana-express-edition/plans/f2dc436a-d851-4c22-a2ba-9de07db7a9ac?tab=instructions
+        # But we are setting these kernel parameters to be TEMPORARY. After a reboot, they will be
+        # reset to their original values.
+        printf "\n\n${PURPLE}SAPA HANA requires some kernel parameters to be configured so it can run properly"
+        printf "\nYou will be asked for your sudo password.\n${RESET}"
+        
+        sudo sysctl -w fs.file-max=20000000
+        sudo sysctl -w fs.aio-max-nr=262144
+        sudo sysctl -w vm.memory_failure_early_kill=1
+        sudo sysctl -w vm.max_map_count=135217728
+        sudo sysctl -w net.ipv4.ip_local_port_range="40000 60999"
+
+        printf "\n${PURPLE}Kernel variables set.\n\n${RESET}"
+    fi
     docker-compose -f docker-compose-$1.yml stop
     docker-compose -f docker-compose-$1.yml rm -f
     docker-compose -f docker-compose-$1.yml up
-fi
