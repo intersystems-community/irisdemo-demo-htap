90caec4753d1b3b5d67392fbc3ed6a7b6368b15d
Amir Samary <amirsamary@gmail.com>
2020-01-20 16:29:22 -0500

    Removing env.sh

--- ICM/ICMDurable/env.sh
+++
@@ -1,8 +0,0 @@
-export SSH_DIR=/ICMDurable/keys
-export TLS_DIR=/ICMDurable/keys
-export ICM_LABEL=asamary
-export IRIS_HOSTNAME=iris-asamary-DM-IRISSpeedTest-0001.weave.local
-export IRIS_ECP_HOSTNAME=iris-asamary-DM-IRISSpeedTest-0001.weave.local
-export HTAP_INGESTION_WORKERS=1
-export HTAP_QUERY_WORKERS=1
-export MAX_CN=6
