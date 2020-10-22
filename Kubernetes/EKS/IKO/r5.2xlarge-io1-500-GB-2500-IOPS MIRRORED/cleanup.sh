ce038ff14c8ceea192f313025193cdc4167f9033
Angel Lopez <angellopezque@gmail.com>
2020-10-22 12:33:12 -0400

    added configuration for mirrored deployment

---
+++ Kubernetes/EKS/IKO/r5.2xlarge-io1-500-GB-2500-IOPS MIRRORED/cleanup.sh
@@ -0,0 +1,29 @@
+source ../utils.sh
+
+kubectl delete -f ../deployment-ui.yml
+exit_if_error "Could not delete deployment-ui.yml"
+
+kubectl delete -f ../deployment-master.yml
+exit_if_error "Could not delete deployment-master.yml"
+
+kubectl delete -f ../deployment-workers.yml
+exit_if_error "Could not delete deployment-workers.yml"
+
+kubectl delete -f ./storage-class-iris*.yml
+exit_if_error "Could not delete storage-class.yml"
+
+kubectl delete -f ../deployment-iris-mirror.yml
+exit_if_error "Could not apply deployment-iris-mirror.yml"
+
+kubectl delete svc/iris-svc
+exit_if_error "Could not delete svc iris-svc"
+
+kubectl delete pvc --all
+exit_if_error "Could not delete pvc"
+
+kubectl delete secret iris-key-secret
+exit_if_error "Could not delete secret key"
+
+helm delete intersystems
+exit_if_error "Error with chart installation for iris kubernetes operator"
+
